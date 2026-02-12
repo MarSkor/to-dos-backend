9acea494fb918f133941c9fbca045cc7fb642b9c
Martine <maskor_@outlook.com>
2026-02-12 19:21:52 +0100

    implement GET all todos route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import todosRouter from "./routes/todos.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 3000;
 
-const router = express.Router();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.use("/api/todos", todosRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to the to-do API");
