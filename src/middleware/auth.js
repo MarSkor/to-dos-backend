ff99535f07cca072483e84a9ef3661432f4c789b
Martine <maskor_@outlook.com>
2026-02-13 17:49:00 +0100

    implement JWT auth and refactor project structure

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const authenticate = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ error: "Access denied." });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.clearCookie("token");
+    return res.status(401).json({ error: "Invalid token." });
+  }
+};
+
+export default authenticate;
